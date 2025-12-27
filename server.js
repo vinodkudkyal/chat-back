cdb851eca7793496b4e416300054ccf82c94faa0
vinodkudkyal <vinodkudkyal2005@gmail.com>
2025-12-27 18:33:15 +0530

    deploye 1

--- server.js
+++ server.js
@@ -598,9 +598,18 @@ require("dotenv").config();
 
 const app = express();
 const server = http.createServer(app);
+
+// const io = new Server(server, {
+//   cors: { origin: "http://localhost:5173" },
+// });
 const io = new Server(server, {
-  cors: { origin: "http://localhost:5173" },
+  cors: {
+    origin: "*", // OR your frontend Render URL later
+    methods: ["GET", "POST"],
+  },
 });
+
+
 
 app.use(cors());
 app.use(express.json());
@@ -845,6 +854,8 @@ io.on("connection", (socket) => {
 
 });
 
-server.listen(5000, () =>
-  console.log("🚀 Server running on http://localhost:5000")
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () =>
+  console.log(`🚀 Server running on port ${PORT}`)
 );
